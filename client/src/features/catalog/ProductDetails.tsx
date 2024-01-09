e6eba7da8b687f002ddef920a44d2b594b55cb09
Jason Parker <>
2024-01-09 17:57:41 +0000

    End of Section 5

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -28,7 +28,7 @@ export default function ProductDetails() {
             <Grid item xs={6}>
                 <Typography variant='h3'>{product.name}</Typography>
                 <Divider sx={{mb:2}}></Divider>
-                <Typography variant='h4' colour='secondary'>${(product.price/100).toFixed(2)}</Typography>
+                <Typography variant='h4'>${(product.price/100).toFixed(2)}</Typography>
                 <TableContainer>
                     <Table>
                         <TableBody>
