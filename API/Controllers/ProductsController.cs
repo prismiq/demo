79e1f47891715386e58d967a07b7068e90e089c8
prismiq <144001111+prismiq@users.noreply.github.com>
2024-01-22 17:35:34 +0000

    main

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,14 +15,18 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy, string searchTerm, string brands, string types) {
+        public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery] ProductParams productParams) {
             var query = _context.Products
-            .Sort(orderBy)
-            .Search(searchTerm)
-            .Filter(brands, types)
+            .Sort(productParams.OrderBy)
+            .Search(productParams.SearchTerm)
+            .Filter(productParams.Brands, productParams.Types)
             .AsQueryable();
 
-            return await query.ToListAsync();
+            var products = await PagedList<Product>.ToPageList(query, productParams.PageNumber, productParams.PageSize);
+
+            Response.AddPaginationHeader(products.MetaData);
+
+            return products;
         }
 
         [HttpGet("{id}")] 
@@ -36,6 +37,14 @@ namespace API.Controllers
             if (product == null) {return NotFound();}
 
             return product;
+        }
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilters() {
+            var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+            var types = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+
+            return Ok(new {brands, types});
         }
     }
 }
